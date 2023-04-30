d19b04e2db27880d5f5fd563c15d33dece2a987b
johnfhima <61589415+johnfhima@users.noreply.github.com>
2023-04-30 15:37:35 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = read_readme("README.md")
 
 setup(
     name='pvbm',
-    version='1.4.0',
+    version='1.5.0',
     packages=find_packages(),
     install_requires=[
         "numpy",
